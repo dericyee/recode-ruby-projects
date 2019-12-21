7cdcf5e7d5c72632abecd1c011a3ec23b5d35e50
dericyee <55132151+dericyee@users.noreply.github.com>
2019-12-21 18:41:23 +0800

    ruby regex

---
+++ exercise_49.rb
@@ -0,0 +1,36 @@
+# Exercise 49
+# Given an input string with alphanumeric characters, extract the integers into a list and print them out in the order of occurrence.
+# If there is a dash (-) immediately before a number, it will be deemed as a negative number and it is a different number from the positive
+# Include the number of times the integer occured in total in the entire string for each integer
+
+# Your output should be:
+# The numbers found are:
+# -23: 3 times
+# 1: 3 times
+# 578: 2 times
+# -12: 3 times
+# 7: 4 times
+# 23: 1 time
+# … etc
+# newHash = Hash.new(0)
+# sentenceSplit.each {|i| newHash[i] +=1 }
+def extract_integer(input_string)
+
+    num = input_string.scan(/(-?\d+)/) 
+    newHash = Hash.new(0)
+    num.each {|i| newHash[i] += 1}
+    
+    # puts newHash
+    newHash.each {|k,v| 
+        if v > 1 
+        puts "#{k.join('')}: #{v} times"
+        elsif v ==1
+        puts  "#{k.join('')}: #{v} time"
+    end}
+
+end
+
+input = "dS-23a1lt578jmnw-1n-23bsi-12p7nbh7asd23mbj578frew1a7oo903o7mk12arr909oposdf435rr"
+
+extract_integer(input)
+
---
+++ exercise_50.rb
@@ -0,0 +1,50 @@
+# Based on your code for Exercise 10, create a Day Organiser
+
+# First, ask for a time as a single input string in military time (e.g. “16:30”) or standard time (e.g. 04:30PM)
+# Then, ask for the activity that will be happening during that time as a string
+# Ask the user if that is the last activity they would like to input (Y/N)
+# If it’s the last activity, then, list out in order of the day, the activity of the day, together with the time in standard time.
+# e.g. Your output should be:
+# Your activities today are:
+# 04:30AM - Wake up, eat breakfast
+# 05:30AM - Go to the gym
+# 08:00AM - Head for Recode Class
+
+def 
+timeArr = []
+activityArr = []
+newHash=Hash.new(0)
+
+loop do
+puts "What is the time now? (In military time)"
+militaryTimeInput = timeArr.push(gets.chomp)
+
+# hour = militaryTimeInput[0] + militaryTimeInput[1]
+# minute = militaryTimeInput[2] + militaryTimeInput[3]
+
+# if hour.to_i > 12
+#     puts "#{militaryTimeInput} in military time is #{hour.to_i - (12)}:#{minute}PM."
+# elsif hour.to_i <= 12
+#     puts "#{militaryTimeInput} in military time is #{hour}:#{minute}AM."
+# end
+
+puts "What is the activity happening during that time?"
+militaryActivity = activityArr.push(gets.chomp)
+
+puts "Is this the last activity you would like to input (Y/N)"
+lastActivity = gets.chomp.upcase
+
+if lastActivity == "Y"
+    puts "Your activities today are:"
+     timeArr.each{|i|newHash[i]= activityArr.each{|j|j}}
+     p timeArr
+     p activityArr
+     p newHash
+
+    break
+end
+
+
+end
+
+
