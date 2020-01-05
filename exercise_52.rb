04843cb4e117e9ce354de60f57b8a35ea0e48ccf
deric.yee@gmail.com <deric.yee@gmail.com>
2020-01-05 12:37:10 +0800

    correction

--- exercise_52.rb
+++ exercise_52.rb
@@ -3,38 +3,14 @@
 # You may not use any Array or Enum methods except length or size
 # You may not use any Enum derived iterations, only for, while or until
 
-	def selection_sort(searchfrom)
-		n = searchfrom.length-1
-		
-		n.times do |i|
-			lowest_index = i
-		for j in i+1..n
-				lowest_index = j if searchfrom[j] < searchfrom[lowest_index]
-			end
-			searchfrom[i],searchfrom[lowest_index] = searchfrom[lowest_index],searchfrom[i] if lowest_index != i
-		end
-	return searchfrom
-end
-
-# searchfrom = [6,29,18,2,72,19,18,10,37]
-# p selection_sort(searchfrom)
-
 def linear_search(target, searchfrom)
-	searchfrom = selection_sort(searchfrom)
-	low = 0
-	high = searchfrom.length-1
 
-	while low<high do
-		mid = (high+low)/2
-	if searchfrom[mid] > target
-		high = mid-1
-	elsif searchfrom[mid] < target
-		low = mid+1
-	elsif searchfrom[mid] == target
-		return mid-1
+	for i in 0..searchfrom.length-1
+		if target == searchfrom[i]
+			return i
+		end
 	end
-end
-		return -1
+	return -1
 end
 
 random_numbers = [6,29,18,2,72,19,18,10,37]
